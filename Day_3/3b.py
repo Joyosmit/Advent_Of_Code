ebd1f9477f83a147366f3f39924de3321ca11c0b
Joyosmit <joyosmitp@gmail.com>
2024-12-03 17:17:52 +0530

    Test

--- Day_3/3b.py
+++ Day_3/3b.py
@@ -7,7 +7,6 @@ def check(line:str) -> Tuple[int, bool]:
     ans = True
     print(line)
     if line[0]!="(":
-        print("BBBa")
 
         ans = False
         return (-1,ans)
@@ -15,7 +14,6 @@ def check(line:str) -> Tuple[int, bool]:
     for ch in line[1:]:
         if ch!=",":
             if not ch.isdigit():
-                print("GAAYY")
                 return (-1, False)
             num1+=ch
         else:
@@ -27,10 +25,8 @@ def check(line:str) -> Tuple[int, bool]:
             break
         num2+=ch
     if line[comma_ind+1+len(num2)]!=")":
-        print("Ya")
         return (-1,False)
     if len(num1)>3 or len(num2)>3:
-        print("SHIT")
         return (-1, False)
     num1 = int(num1)
     num2 = int(num2)
